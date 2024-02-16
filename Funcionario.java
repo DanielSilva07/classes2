6c19ff3b5c0b113f8a040b47ea77cbab6670b1c4
danielsilva07 <daniel97silva.ds@gmail.com>
2024-02-16 16:56:54 -0300

    new

---
+++ Funcionario.java
@@ -0,0 +1,34 @@
+public class Funcionario {
+
+
+    private  String nome;
+    private String localdeTrabalho;
+    private int codigo;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLocaldeTrabalho() {
+        return localdeTrabalho;
+    }
+
+    public void setLocaldeTrabalho(String localdeTrabalho) {
+        this.localdeTrabalho = localdeTrabalho;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+}
+
---
+++ Primeiraclasse.java
@@ -0,0 +1,24 @@
+
+public class Primeiraclasse {
+
+
+    public static void main( String Args []) {
+
+        Funcionario Funcionario = new Funcionario();
+        Funcionario.setCodigo(77);
+        Funcionario.setLocaldeTrabalho("Rio de Janeiro");
+        Funcionario.setNome("Tião");
+
+        // Mais uma tarefa concluida //
+
+        System.out.println(Funcionario.getCodigo());
+        System.out.println(Funcionario.getLocaldeTrabalho());
+        System.out.println(Funcionario.getNome());
+
+
+    }
+
+
+
+
+}
